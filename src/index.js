b95033f011fe45b6bf6ee085ee071c247eaae3f4
Karin Liegmann <karin.liegmann@googlemail.com>
2021-03-10 14:12:10 +0100

    created GlobalStyles.js and set up files for Styleguidist-Documentation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GlobalStyles } from './GlobalStyles';
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    < GlobalStyles />
+    < Router >
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
