e18df06ce7f43144f690ad5b908a8bce03184842
Karin Liegmann <karin.liegmann@googlemail.com>
2021-03-15 23:23:24 +0100

    created RecipeSearch-page

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,7 @@
 import { useState } from 'react'
+import { Route, Switch } from 'react-router-dom'
+
+import RecipeSearch from './pages/RecipeSearch'
 import Header from './components/Header'
 
 function App() {
@@ -10,6 +13,11 @@ function App() {
       <Header
         open={open}
         setOpen={setOpen} />
+      <main>
+        <Switch>
+          <Route exact path="/" component={RecipeSearch} />
+        </Switch>
+      </main>
 
     </div>
   );
