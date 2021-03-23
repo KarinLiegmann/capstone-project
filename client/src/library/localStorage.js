126ed5c3b393d7da8500611dc70bd6c4a7037519
Karin Liegmann <karin.liegmann@googlemail.com>
2021-03-23 14:14:25 +0100

    updated dependencies, added Button component and localStorage-functions

---
+++ client/src/library/localStorage.js
@@ -0,0 +1,13 @@
+function saveToLocal(key, value) {
+    return (localStorage.setItem(key, JSON.stringify(value)))
+}
+
+function loadFromLocal(key) {
+    try {
+        return JSON.parse(localStorage.getItem(key))
+    } catch (error) {
+        return (error) => console.error(error)
+    }
+}
+
+export { saveToLocal, loadFromLocal }
--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import axios from 'axios';
@@ -7,32 +6,24 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const API_KEY = process.env.API_KEY
-const DB_USER = process.env.DB_USER
-const DB_PW = process.env.DB_PW
+const DB_PATH = process.env.DB_PATH
 
 
-const server = express();
-server.use(cors());
-server.use(express.json());
-server.use(bodyParser.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-const connectionString = `mongodb+srv://${DB_USER}:${DB_PW}@capstone-project.wvg8c.mongodb.net/`;
+
+const connectionString = `${DB_PATH}`;
 mongoose.connect(connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
-server.get('/', (req, res) =>
+app.get('/', (req, res) =>
     res.json({ status: 'Server is up and running.' })
 );
 
-server.get('/ingredients', (req, res) => {
-    axios.get(`https://api.spoonacular.com/food/ingredients/search?apiKey=${API_KEY}&addChildren=true&sortDirection=desc&number=5&query=cheese`)
-        .then(res => res.data)
-        .then(ingredients => res.send(ingredients))
-        .catch(error => res.json(error))
-})
-
 
 const port = 4000;
-server.listen(port, () => console.log(`Server listens on port ${port}.`));
+app.listen(port, () => console.log(`Server listens on port ${port}.`));
