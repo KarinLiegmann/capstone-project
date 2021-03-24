f15d66eb624daea6fda17825b94db7c583e1b8ff
Karin Liegmann <karin.liegmann@googlemail.com>
2021-03-24 15:35:51 +0100

    added basic card-styling, recipe-counter and buttons

--- client/src/components/RecipeCards.js
+++ client/src/components/RecipeCards.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import { img } from '../assets/RecipeCard_Background.png'
+import img from '../assets/RecipeCard_Background.png'
 import { FaHeart } from 'react-icons/fa'
 import { RiDislikeLine } from 'react-icons/ri'
 
@@ -10,33 +10,80 @@ export default function RecipeCard({ recipes }) {
             (<CardWrapper
                 key={recipe.id}
             >
-                <img src={recipe.image} alt={recipe.title} />
-                <h3>{recipe.title}</h3>
-                <DislikeIcon />
-                <LikeIcon />
-                <p>You need:</p>
-                {recipe.missedIngredients.map((missedIngredient) => (
-                    <li>{missedIngredient.original}</li>
-                ))}
-                <p>You have:</p>
-                {recipe.usedIngredients.map((usedIngredient) => (
-                    <li>{usedIngredient.original}</li>
-                ))}
-                <p>Unused Ingredients:</p>
-                {recipe.unusedIngredients.map((usedIngredient) => (
-                    <li>{usedIngredient.name}</li>
-                ))}
-            </CardWrapper>
-            ))}
+                <CardContent>
+                    <img src={recipe.image} alt={recipe.title} />
+                    <h3>{recipe.title}</h3>
+                    <IconsWrapper>
+                        <DislikeIcon />
+                        <LikeIcon />
+                    </IconsWrapper>
+                    <IngredientsWrapper>
+
+                        <ul>
+                            <p>You need:</p>
+                            {recipe.missedIngredients.map((missedIngredient) => (
+                                <li>{missedIngredient.original}</li>
+                            ))}
+                        </ul>
+
+
+                        <ul>
+                            <p>You have:</p>
+                            {recipe.usedIngredients.map((usedIngredient) => (
+                                <li>{usedIngredient.original}</li>
+                            ))}
+                        </ul>
+                    </IngredientsWrapper>
+                </CardContent>
+            </CardWrapper >
+            ))
+            }
 
         </>
     )
 }
 
 const CardWrapper = styled.section`
-border: 1px solid var(--clr-dark);
+background-image: url(${img});
+background-size: 100% auto;
+background-repeat: no-repeat;
+background-position-x: center;
+display: flex;
+justify-content: center;
 margin-bottom: 10px;
+width: 90%;
 `
+const CardContent = styled.div`
+border: 1px solid var(--clr-dark);
+padding: 3rem 1.5rem;
+
+img {
+    border-radius: 10px;
+    width: 60%;
+}
+
+p {    
+    font-weight: var(--fw-bold);
+    margin: 0;
+    text-align: left;
+}
+
+li {
+    text-align: left;
+}
+`
+
+const IngredientsWrapper = styled.div`
+display: flex;
+`
+
+const IconsWrapper = styled.div`
+display: flex;
+justify-content: space-between;
+padding: 0 .5rem;
+`
+
+
 const DislikeIcon = styled(RiDislikeLine)`
 color: var(--clr-accent2-light);
 font-size: var(--fs-h1);
--- client/src/pages/RecipeResults.js
+++ client/src/pages/RecipeResults.js
@@ -2,18 +2,26 @@ import styled from 'styled-components'
 import PropTypes from 'prop-types'
 
 import RecipeCards from '../components/RecipeCards'
+import Button from '../components/Button'
 
 export default function RecipeResults({ recipes }) {
     return (
-        <>
+        <Wrapper>
             <h2>Here is what we found:</h2>
             <RecipeCards
                 recipes={recipes} />
-        </>
+            <h2>Recipes left: {recipes.length} </h2>
+            <Button text="All Done!" />
+            <Button text="Try Again" />
+        </Wrapper>
     )
 }
 
-
+const Wrapper = styled.section`
+align-items: center;
+display: flex;
+flex-direction: column;
+`
 
 RecipeResults.propTypes = {
     recipes: PropTypes.arrayOf(PropTypes.object).isRequired,
