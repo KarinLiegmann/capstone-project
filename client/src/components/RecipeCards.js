56182f17a2f31b0aef00d0f38eb0b100b5b8788c
Karin Liegmann <karin.liegmann@googlemail.com>
2021-04-06 16:49:18 +0200

    commits

--- client/src/components/RecipeCards.js
+++ client/src/components/RecipeCards.js
@@ -80,7 +80,7 @@ const CardContent = styled.div`
 background: #fff;
 border-radius: 15px;
 box-shadow: 2px 5px 10px rgba(0, 0, 0, .4);
-padding: 3.2rem 2rem;
+padding: .5rem 1rem;
 margin-bottom: -17rem;
 margin-top: -11.5rem;
 max-width: 320px;
